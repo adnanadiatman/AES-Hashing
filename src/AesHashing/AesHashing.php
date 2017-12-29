b654c4aa74c8859acc384b75bfe15e2344b06f56
Adnan Adiatman <adnanadiatman@gmail.com>
2017-12-29 19:28:28 +0700

    done

---
+++ src/AesHashing/AesHashing.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AesHashing;
+
+class AesHashing
+{
+    static function acrypt($password)
+    {
+        $plaintext = str_random(8);
+        $cipher = "aes-256-gcm";
+
+        $ivlen  = openssl_cipher_iv_length($cipher);
+        $iv     = openssl_random_pseudo_bytes($ivlen);
+        $hashed = openssl_encrypt($plaintext, $cipher, $password, $options = 0, $iv, $tag);
+
+        return $plaintext . "." . base64_encode($iv) . "." . base64_encode($hashed);
+    }
+
+    static function checkAcrypt($password, $hashed)
+    {
+        $components = explode(".", $hashed);
+        $cipher = "aes-256-gcm";
+
+        if(count($components)!=3){
+            return false;
+        }
+
+        $result = openssl_decrypt(base64_decode(@$components[2]), $cipher, $password, $options = 0, base64_decode(@$components[1]), $tag);
+
+        if($result===$components[0]){
+            return true;
+        }
+
+        return false;
+    }
+}
